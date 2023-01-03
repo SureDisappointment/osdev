876352ef0ce44e49bd76ac9fdfe18891e311fad2
Luca Schneider <dev.schneider@proton.me>
2023-01-03 05:50:51 +0100

    make ps2kbd use the keyboard controller

--- src/impl/x86_64/device/ps2_keyboard.c
+++ src/impl/x86_64/device/ps2_keyboard.c
@@ -2,10 +2,21 @@
 #include "plugbox.h"
 #include "pic.h"
 #include "cgascr.h"
+#include "key.h"
+#include "keyctrl.h"
+
+Key key = {0, 0, 0};
 
 void interrupt_routine()
 {
-    CGA_puts("test");
+    key = keyctrl_key_hit();
+    if (!key_valid(key))
+        return;
+
+    if (key_ctrl(key) && key_alt(key) && key.scancode == key_del)
+        keyctrl_reboot();
+    
+    CGA_putchar(key.ascii);
 }
 
 void ps2kbd_plugin()
