42f7b46db1976d944deaabecaf0c4ee794c23a3b
Luca Schneider <dev.schneider@proton.me>
2023-01-16 23:00:18 +0100

    Reminder for future work on exception error codes

--- src/impl/kernel/guardian.c
+++ src/impl/kernel/guardian.c
@@ -5,6 +5,17 @@ extern void guardian(unsigned int slot, unsigned int *error_code);
 
 void guardian(unsigned int slot, unsigned int *error_code)
 {
+    int e = 0;
+    if(slot == 8
+        || (slot >= 10 && slot <= 14)
+        || slot == 17
+        || slot == 21
+        || slot == 29
+        || slot == 30)
+    {
+        e = *error_code;
+    }
+    // TODO: pass error code to exceptions
     interrupt_handler *gate = plugbox_report(slot);
     if(gate->prologue())
         guard_relay(gate);
